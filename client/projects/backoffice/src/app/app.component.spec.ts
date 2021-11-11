cd403afb055a29b6cc914dd7d8370f3c43d7f0cd
Jason White <jmw5598@gmail.com>
2021-11-11 07:12:36 -0500

    rename manager project to backoffice, setup scripts to start admin and backoffice on separate port, setup build scripts, setup alias

--- client/projects/backoffice/src/app/app.component.spec.ts
+++ client/projects/backoffice/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'manager'`, () => {
+  it(`should have as title 'backoffice'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('manager');
+    expect(app.title).toEqual('backoffice');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('manager app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('backoffice app is running!');
   });
 });
