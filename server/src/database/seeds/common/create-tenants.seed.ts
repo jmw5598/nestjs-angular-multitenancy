a91a298e03966345883ec1fb389a94ea42fe438b
Jason White <jmw5598@gmail.com>
2021-11-13 16:21:21 -0500

    wip: added typeorm-seed package for seeding database, created seeds for tenants, and users

---
+++ server/src/database/seeds/common/create-tenants.seed.ts
@@ -0,0 +1,39 @@
+import { Tenant } from "src/database/entities/common/tenant.entity";
+import { Connection } from "typeorm";
+import { Factory, Seeder } from "typeorm-seeding";
+import { v4 as uuid } from 'uuid';
+
+const tenantSeeds: Tenant[] = [
+  {
+    createdAt: new Date(),
+    updatedAt: new Date(),
+    identifier: uuid(), 
+    domain: 'localhost', 
+    connectionString: 'postgresql://tenant1:tenant1@localhost:5432/tenant_1'
+  } as Tenant,
+  {
+    createdAt: new Date(),
+    updatedAt: new Date(),
+    identifier: uuid(), 
+    domain: 'tenant1', 
+    connectionString: 'postgresql://tenant1:tenant1@localhost:5432/tenant_1'
+  } as Tenant,
+  {
+    createdAt: new Date(),
+    updatedAt: new Date(),
+    identifier: uuid(), 
+    domain: 'tenant2', 
+    connectionString: 'postgresql://tenant2:tenant2@localhost:5432/tenant_2'
+  } as Tenant,
+];
+
+export default class CreateTenants implements Seeder {
+  public async run(factory: Factory, connection: Connection): Promise<any> {
+    await connection
+      .createQueryBuilder()
+      .insert()
+      .into(Tenant)
+      .values(tenantSeeds)
+      .execute();
+  }
+}
---
+++ server/src/database/seeds/tenant/create-users.seed.ts
@@ -0,0 +1,34 @@
+import { User } from "src/database/entities/tenant/user.entity";
+import { Connection } from "typeorm";
+import { Factory, Seeder } from "typeorm-seeding";
+import { v4 as uuid } from 'uuid';
+
+const userSeeds: User[] = [
+  {
+    createdAt: new Date(),
+    updatedAt: new Date(),
+    username: 'dev@dev.com',
+    password: '$2a$12$DKomgaQ50SaFm/LFpWBgCeFBxYLmjlauhuJ4n5MtvhbEGUY24M3re',
+    resetToken: uuid(),
+    resetTokenExpiration: new Date()
+  } as User,
+  {
+    createdAt: new Date(),
+    updatedAt: new Date(),
+    username: 'dev2@dev.com',
+    password: '$2a$12$DKomgaQ50SaFm/LFpWBgCeFBxYLmjlauhuJ4n5MtvhbEGUY24M3re',
+    resetToken: uuid(),
+    resetTokenExpiration: new Date()
+  } as User,
+];
+
+export default class CreateUsers implements Seeder {
+  public async run(factory: Factory, connection: Connection): Promise<any> {
+    await connection
+      .createQueryBuilder()
+      .insert()
+      .into(User)
+      .values(userSeeds)
+      .execute();
+  }
+}
