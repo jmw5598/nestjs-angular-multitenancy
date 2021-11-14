343a95f555711d3ce2acc903fe82d9e6aa9d11f3
Jason White <jmw5598@gmail.com>
2021-11-14 06:21:05 -0500

    wip: added logging out functionality

--- client/projects/admin/src/app/modules/authentication/pages/logging-out/logging-out.component.ts
+++ client/projects/admin/src/app/modules/authentication/pages/logging-out/logging-out.component.ts
@@ -1,5 +1,6 @@
 import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { AuthenticationStore } from '@xyz/admin/modules/core/store/authentication.store';
 
 @Component({
   selector: 'xyz-logging-out',
@@ -9,10 +10,14 @@ import { Router } from '@angular/router';
 })
 export class LoggingOutComponent implements OnInit {
 
-  constructor(private _router: Router) { }
+  constructor(
+    private _router: Router,
+    private _authenticationStore: AuthenticationStore
+  ) { }
 
   ngOnInit(): void {
     setTimeout(() => {
+      this._authenticationStore.logoutUser();
       this._router.navigate(['/auth', 'login']);
     }, 2000);
   }
