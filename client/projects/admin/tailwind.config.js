e5f73ef1b9acf591f7ec5e096d4959eb8d788e30
Jason White <jmw5598@gmail.com>
2021-11-16 16:08:12 -0500

    wip: change login layout, change primary color, created color scheme with ada designs systems, still have to update colors other than primary

--- client/projects/admin/tailwind.config.js
+++ client/projects/admin/tailwind.config.js
@@ -2,16 +2,23 @@ const colors = require('tailwindcss/colors')
 
 module.exports = {
   prefix: '',
-  purge: {
-    content: [
-      './src/**/*.{html,ts}',
-    ]
-  },
+  purge: ['./src/**/*.{html,ts}'],
   darkMode: 'class', // or 'media' or 'class'
   theme: {
     extend: {
       colors: {
-        primary: colors.indigo,
+        'primary': {
+          50: '#E0F6FE',
+          100: '#E0F6FE',
+          200: '#C2EAFD',
+          300: '#A3D9FA',
+          400: '#8BC7F6',
+          500: '#65ADF1',
+          600: '#4987CF',
+          700: '#3265AD',
+          800: '#20468B',
+          900: '#133173',
+        },
         secondary: colors.gray,
         success: colors.green,
         info: colors.blue,
