a266ea530be72265f347cfaaa4c259732965633d
Jason White <jmw5598@gmail.com>
2021-11-16 05:42:38 -0500

    wip: updated create-user seed

--- server/src/database/seeds/tenant/create-users.seed.ts
+++ server/src/database/seeds/tenant/create-users.seed.ts
@@ -1,5 +1,10 @@
+import { isEmail } from "class-validator";
+import { connect } from "http2";
+import { Address } from "src/database/entities/tenant/address.entity";
+import { Profile } from "src/database/entities/tenant/profile.entity";
+import { Role } from "src/database/entities/tenant/role.entity";
 import { User } from "src/database/entities/tenant/user.entity";
-import { Connection } from "typeorm";
+import { Connection, Repository } from "typeorm";
 import { Factory, Seeder } from "typeorm-seeding";
 import { v4 as uuid } from 'uuid';
 
@@ -10,7 +15,9 @@ const userSeeds: User[] = [
     username: 'dev@dev.com',
     password: '$2a$12$DKomgaQ50SaFm/LFpWBgCeFBxYLmjlauhuJ4n5MtvhbEGUY24M3re',
     resetToken: uuid(),
-    resetTokenExpiration: new Date()
+    resetTokenExpiration: new Date(),
+    isEmailConfirmed: true,
+    isLockedOut: false
   } as User,
   {
     createdAt: new Date(),
@@ -18,7 +25,9 @@ const userSeeds: User[] = [
     username: 'dev2@dev.com',
     password: '$2a$12$DKomgaQ50SaFm/LFpWBgCeFBxYLmjlauhuJ4n5MtvhbEGUY24M3re',
     resetToken: uuid(),
-    resetTokenExpiration: new Date()
+    resetTokenExpiration: new Date(),
+    isEmailConfirmed: true,
+    isLockedOut: false
   } as User,
 ];
 
