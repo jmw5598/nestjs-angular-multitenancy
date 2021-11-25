6663f7d7a231d4ba70a3071df4a5851ce02fc863
Jason White <jmw5598@gmail.com>
2021-11-25 08:40:14 -0500

    wip: created calendar-event-types controller

--- server/src/calendar/controllers/calendar-event-types.controller.ts
+++ server/src/calendar/controllers/calendar-event-types.controller.ts
@@ -1,5 +1,6 @@
-import { Controller, Get, UseGuards } from '@nestjs/common';
+import { Controller, Get, Request, UseGuards } from '@nestjs/common';
 import { JwtAuthenticationGuard } from 'src/authentication/guards/jwt-authentication.guard';
+import { Tenant } from 'src/database/entities/common/tenant.entity';
 import { CalendarEventType } from 'src/database/entities/tenant/calendar-event-type.entity';
 import { CalendarEventTypesService } from '../services/calendar-event-types.service';
 
@@ -11,7 +12,8 @@ export class CalendarEventTypesController {
   ) { }
 
   @Get()
-  public async getAllCalendarEventTypes(): Promise<CalendarEventType[]> {
+  public async getAllCalendarEventTypes(@Request() request): Promise<CalendarEventType[]> {
+    const tenant: Tenant = request.tenant as Tenant;
     return this._calendarEventTypesService.getCalendarEventTypes();
   }
 }
